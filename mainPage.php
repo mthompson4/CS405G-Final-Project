336cb1cf6823014ef9460c8023048b05dd7f2481
RadRedPanda <kzh227@g.uky.edu>
2018-04-18 19:48:31 -0400

    lotta stuff
    added cookies, formatted a bit, and added search stuff

--- mainPage.php
+++ mainPage.php
@@ -1,3 +1,88 @@
+<?php
+	$serverName = 		"127.0.0.1";
+	$serverUserName = 	"admin";
+	$serverPassword = 	"3j2l3j2klb3b2klb32l";
+	$dbName = 			"bookstore";
+	
+	$conn = mysqli_connect($serverName, $serverUserName, $serverPassword, $dbName);
+	
+	if(!$conn){
+		die("Connection Failed: ".mysqli_connect_error());
+	}
+
+	if(isset($_GET["buttonPressed"]) && $_GET["buttonPressed"] == "Register"){
+		$currentEmail = 	$_POST["newUserEmail"];
+		$currentPass = 		$_POST["newUserPassword"];
+		$currentFName = 	$_POST["newUserFName"];
+		$currentMName = 	$_POST["newUserMName"];
+		$currentLName = 	$_POST["newUserLName"];
+		$currentAge = 		$_POST["newUserAge"];
+		$currentGender = 	$_POST["newUserGender"];
+		$currentUserType = "Customer";
+		
+		setcookie("user", $currentEmail);
+		setcookie("pass", $currentPass);
+		
+		$sql = "INSERT INTO users(fname, mname, lname, email, password, age, gender, user_type)
+				VALUES('$currentFName', '$currentMName', '$currentLName', '$currentEmail', '$currentPass', '$currentAge', '$currentGender', '$currentUserType')";
+		
+		mysqli_query($conn, $sql);
+		
+		mysqli_close($conn);
+		
+	}else if(isset($_GET["buttonPressed"]) && $_GET["buttonPressed"] == "Login"){
+		$currentEmail = base64_decode(htmlspecialchars($_GET["user"]));
+		$currentPass = base64_decode(htmlspecialchars($_GET["pass"]));
+		
+		setcookie("user", $currentEmail);
+		setcookie("pass", $currentPass);
+		
+		$sql = 'SELECT fname, mname, lname, age, gender, user_type FROM users WHERE email="'.$currentEmail.'" AND password="'.$currentPass.'"';
+		
+		$results = mysqli_query($conn, $sql);
+		
+		if(mysqli_num_rows($results) > 0){
+			
+			$row = mysqli_fetch_assoc($results);
+			$currentFName = 	$row["fname"];
+			$currentMName = 	$row["mname"];
+			$currentLName = 	$row["lname"];
+			$currentAge = 		$row["age"];
+			$currentGender = 	$row["gender"];
+			$currentUserType = 	$row["user_type"];
+		
+		}
+	}else{
+		if(!isset($_COOKIE["user"])){
+			//no cookie
+		}else{
+			//cookie
+			$currentEmail = $_COOKIE["user"];
+			$currentPass = 	$_COOKIE["pass"];
+			
+			$sql = 'SELECT fname, mname, lname, age, gender, user_type FROM users WHERE email="'.$currentEmail.'" AND password="'.$currentPass.'"';
+			$results = mysqli_query($conn, $sql);
+			
+			if(mysqli_num_rows($results) > 0){
+				
+				$row = mysqli_fetch_assoc($results);
+				$currentFName = 	$row["fname"];
+				$currentMName = 	$row["mname"];
+				$currentLName = 	$row["lname"];
+				$currentAge = 		$row["age"];
+				$currentGender = 	$row["gender"];
+				$currentUserType = 	$row["user_type"];
+			
+			}
+		}
+		if(isset($_GET["buttonPressed"]) && $_GET["buttonPressed"] == "Search"){
+			$searchTerm = $_GET["search"];
+			echo $searchTerm;
+		}
+	}
+	
+?>
+
 <html>
 <head>
 	<style>
@@ -9,77 +94,47 @@
 </head>
 <body>
 
-<div>
-	<center><h1><font size="7">Bookstore Database</font></h1></center>
-</div>
+<div><center><h1><font size="7">Bookstore Database</font></h1></center></div>
 
-<?php
-	
-	$serverName = "127.0.0.1";
-	$serverUserName = "admin";
-	$serverPassword = "3j2l3j2klb3b2klb32l";
-	$dbName = "bookstore";
-	
-	$conn = mysqli_connect($serverName, $serverUserName, $serverPassword, $dbName);
-	
-	if(!$conn){
-		die("Connection Failed: ".mysqli_connect_error());
-	}
-	
-	function showManagerOptionButton(){
-		echo '<form method="post">';
-		echo '<h1><input type="submit" name="managerOptions" value="Manager Options" /></h1><br/>';
-		echo '</form>';
-	}
-	
-	if($_GET["buttonPressed"] == "Register"){
-		$currentEmail = $_POST["newUserEmail"];
-		$currentPass = $_POST["newUserPassword"];
-		$currentFName = $_POST["newUserFName"];
-		$currentMName = $_POST["newUserMName"];
-		$currentLName = $_POST["newUserLName"];
-		$currentAge = $_POST["newUserAge"];
-		$currentGender = $_POST["newUserGender"];
-		$currentUserType = "Customer";
-		
-		echo "<h1>Hi, $currentFName!</h1>";
-		
-		$sql = "INSERT INTO users(fname, mname, lname, email, password, age, gender, user_type)
-				VALUES('$currentFName', '$currentMName', '$currentLName', '$currentEmail', '$currentPass', '$currentAge', '$currentGender', '$currentUserType')";
-		
-		mysqli_query($conn, $sql);
-		
-		mysqli_close($conn);
-		
-	}
-	else{
-		$currentEmail = base64_decode(htmlspecialchars($_GET["user"]));
-		$currentPass = base64_decode(htmlspecialchars($_GET["pass"]));
-		
-		$sql = 'SELECT fname, mname, lname, age, gender, user_type FROM users WHERE email="'.$currentEmail.'" AND password="'.$currentPass.'"';
-		
-		$results = mysqli_query($conn, $sql);
-		
-		if(mysqli_num_rows($results) > 0){
-			
-			$row = mysqli_fetch_assoc($results);
-			$currentFName = $row["fname"];
-			$currentMName = $row["mname"];
-			$currentLName = $row["lname"];
-			$currentAge = $row["age"];
-			$currentGender = $row["gender"];
-			$currentUserType = $row["user_type"];
-		
+<form method="get">
+	<font size="5">Basic Search: </font>
+	<input type="text" name="search" style="width: 200px; height: 40px">	
+	<input type="submit" name="buttonPressed" value="Search" style="width: 150px; height: 40px; font-size:12pt">
+	<input type="submit" name="buttonPressed" id="test" value="Advanced" style="width: 150px; height: 40px; font-size:12pt"/><br/>
+	<?php
+		function advSearch(){
+			?>
+			<font size="5">Author: </font>
+			<input type="text" name="author" style="width: 200px; height: 40px">	
+			<font size="5">Subject: </font>
+			<input type="text" name="subject" style="width: 200px; height: 40px">	
+			<font size="5">Date Published: </font>
+			<input type="text" name="search" style="width: 200px; height: 40px">	
+			<font size="5">Price: </font>
+			<input type="number" name="search" style="width: 200px; height: 40px">	
+			<?php
 		}
-		
-		echo "<h1>Hi, $currentFName!</h1>";
-		
+
+		if(isset($_GET["buttonPressed"]) && $_GET["buttonPressed"] == "Advanced"){
+		   advSearch();
+		}
+	?>
+</form>
+<?php	
+	function showManagerOptionButton(){
+		?>
+		<form method="post" action="managerOptions.php">
+		<h1><input type="submit" name="managerOptions" value="Manager Options" /></h1><br/>
+		</form>
+		<?php
 	}
 	
-	if($currentUserType == "Manager"){
+	if(isset($currentUserType) && $currentUserType == "Manager"){
 		showManagerOptionButton();
 	}
-	
+	if(isset($currentFName)){
+		echo "<h1>Hi, ".$currentFName."!</h1>";
+	}
 	//echo "$currentUser".'</br>';
 	//echo "$currentPass".'</br>';
 	
